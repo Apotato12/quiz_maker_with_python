f87c01cff169126af6ff3d8a6c58cf994a1179c3
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-10 01:52:46 +0800

    made_question_and_answer_into_functions_for_easier_seperation

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,11 +1,14 @@
 import os
 
-answers = []
-def answers():
-    answers['a'] = input("Please enter option a: ")
-    answers['b'] = input("Please enter option b: ")
-    answers['c'] = input("Please enter option c: ")
-    answers['d'] = input("Please enter option d: ")
 
-while True:
-     question = input("enter a question:")
+def options_for_question():
+    answers = []
+    def answers():
+        answers['a'] = input("Please enter option a: ")
+        answers['b'] = input("Please enter option b: ")
+        answers['c'] = input("Please enter option c: ")
+        answers['d'] = input("Please enter option d: ")
+
+def ask_for_the_question():
+    while True:
+        question = input("enter a question:")
