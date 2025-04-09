8145fdf65de90c4fd4f0898cf57e77dcaf872679
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-10 01:45:44 +0800

    added_set_to_store_a,b,c,d_options

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,4 +1,11 @@
 import os
 
+answers = []
+def answers():
+    answers['a'] = input("Please enter option a: ")
+    answers['b'] = input("Please enter option b: ")
+    answers['c'] = input("Please enter option c: ")
+    answers['d'] = input("Please enter option d: ")
+
 while True:
      question = input("enter a question:")
