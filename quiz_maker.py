ba0b8633a113920eee0886db4d7883662a6d969b
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-10 01:30:31 +0800

    pseudocode

---
+++ quiz_maker.py
@@ -0,0 +1,10 @@
+#Prompt the user for a quiz name
+#import os to see if a file named quiz.txt exists
+#if it doesn't exist create a fale named quiz.txt
+#prompt the user to enter a question
+#after the user enters a question prompt them to enter possible answers and store them in the variables a.b.c.d
+#after the user enters a question use the variables a,b,c,d to store the answers
+#after the variables are stored prompt the user to enter the letter correct answer
+#ask the user if they want to add another question
+#if yes repeat step 1 until step 9
+#if no write the data collected on the text file(quiz.txt)
