50da32dcb9bd6f728cd1bbff17a7a447875bd542
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-10 16:09:22 +0800

    merged_the_answer_with_question_so_because_it_does_not_show_up_if_inserted_seperately

--- quiz_maker.py
+++ quiz_maker.py
@@ -2,20 +2,9 @@ import os
 
 file_name = "quiz.txt"
 
-def options_for_question():
-    answers = []
-    def answers():
-        answers['a'] = input("Please enter option a: ")
-        answers['b'] = input("Please enter option b: ")
-        answers['c'] = input("Please enter option c: ")
-        answers['d'] = input("Please enter option d: ")
-        correct_answer = input("Please enter the correct answer (a, b, c, or d): ")
-
-        return answers, correct_answer
-
-
 def ask_for_the_question():
     list_of_questions = []
+    answers = {}
     while True:
         question = input("enter a question:")
         if question in list_of_questions:
@@ -25,7 +14,13 @@ def ask_for_the_question():
         list_of_questions.append(question)
         print ("question has been added")
 
-        answers = options_for_question()
+        answers['a'] = input("Please enter option a: ")
+        answers['b'] = input("Please enter option b: ")
+        answers['c'] = input("Please enter option c: ")
+        answers['d'] = input("Please enter option d: ")
+        correct_answer = input("Please enter the correct answer (a, b, c, or d): ")
+
+        return answers, correct_answer
 
         new_question = input("Do you want to add another question? (y or n): ")
         if new_question == "y":
