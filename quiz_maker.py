88620b88753f56106bfb0eabb9b04ea1f95fd12c
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-10 15:29:10 +0800

    moved_the_correct_answer_to_the_option_for_questions_fucntion

--- quiz_maker.py
+++ quiz_maker.py
@@ -9,16 +9,14 @@ def options_for_question():
         answers['b'] = input("Please enter option b: ")
         answers['c'] = input("Please enter option c: ")
         answers['d'] = input("Please enter option d: ")
-
-
-def correct_answer():
-    correct_answer = input("Please enter the correct answer (a, b, c, or d): ")
-    
+        correct_answer = input("Please enter the correct answer (a, b, c, or d): ")
 
 
 def ask_for_the_question():
     while True:
         question = input("enter a question:")
+        answers = options_for_question()
+
 
 def make_file():
     if not os.path.exists(file_name):
