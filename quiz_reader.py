78a708e9a76b68ba53ccace2316dfa649358b881
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-01 22:09:44 +0800

    added_modules

--- quiz_reader.py
+++ quiz_reader.py
@@ -1,15 +1,2 @@
-#import os and random
-#define the file name that the program will open(quiz.txt)
-#use the os the check if the file name exists
-#if there is no file name print "file does not exist"
-#if there is a file with the name quiz.txt open the file and read the contents
-#after reading the file read the questions ,answers and correct answer from the file and store them in a dictionary
-#(each question and answer should be seperated by a new line)
-#use the random module to shuffle the questions and answers
-#print the questions and answers to the user and make them choose which one is the correct answer
-#if the user chooses the correct answer print "correct" and add 1 point to the score 
-#if not print "incorrect" and don't add any points to the score
-#repeat until no more questions are left
-#print the score of the user and the total number of questions
-#ask the user if they want to play again
-#if yes repeat if no stop the program
+import os
+import random
