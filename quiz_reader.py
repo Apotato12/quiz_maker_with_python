7447c14651fc8c53f71a2a0dfdf9703f56e18376
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-01 22:57:30 +0800

    added_file_checker_to_see_if_it_exists

--- quiz_reader.py
+++ quiz_reader.py
@@ -2,3 +2,9 @@ import os
 import random
 
 file_name = "quiz.txt"
+
+def check_if_file_exists():
+    if os.path.exists(file_name):
+        print("Quiz file exists")
+    else:
+        print("Quiz file does not exist. Please make one")
