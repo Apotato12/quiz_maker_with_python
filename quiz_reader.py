afc64dbf5ddd071e2847417580dc188ab488c206
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-05 10:43:52 +0800

    changed_question_reader_to_quiz_reader_because_it_has_parts_of_all_3_and_makes_more_sense_to_call_it_quiz_reader

--- quiz_reader.py
+++ quiz_reader.py
@@ -9,7 +9,7 @@ def check_if_file_exists():
     else:
         print("Quiz file does not exist. Please make one")
 
-def question_reader():
+def quiz_reader():
     questions = []
     with open(file_name, "r") as file:
         lines = file.readlines()
